e9582ccd999514d0f5fdcd446a9762f00f2554e9
revat <revat@DESKTOP-2E3NR6Q>
2023-02-18 22:06:26 +0530

    First check

---
+++ src/Test001.java
@@ -0,0 +1,17 @@
+
+public class Test001 {
+
+	public static void main(String[] args) {
+		String str="Durva";
+		if(str=="Durva")
+		{
+			System.out.println("Durva Mali");
+		}
+		else
+		{
+			System.out.println("Revati");
+		}
+
+	}
+
+}
